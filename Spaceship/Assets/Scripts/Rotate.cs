f573b78f0739df7d7625299316a92ef408f7766e
Curtis Phillips <cmontphillips@yahoo.com>
2020-01-23 13:09:07 -0800

    Added Asteroid Prefabs
    -Added folders for Normal Maps, Fonts, and moved files accordingly.
    -Added rotate script for rotating prefabs
    -Added 3 asteroid meshes and made prefabs for them.

---
+++ Spaceship/Assets/Scripts/Rotate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public Vector3 rotate;
+    public float rotationSpeed = 1;
+    public bool randomize;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (randomize)
+            rotate = new Vector3(Random.Range(-2, 2), Random.Range(-1, 1), Random.Range(-2, 2));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotate * rotationSpeed);
+    }
+}
